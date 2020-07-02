d48b894999da1268c696f5e16ff2ca407ef54936
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-07-02 17:01:46 +0100

    feat: is two

--- apps/core-api/index.js
+++ apps/core-api/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { isOne } = require('@steviebiddles/validator');
+const { isOne, isTwo } = require('@steviebiddles/validator');
 
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
     console.log(isOne(1));
+    console.log(isTwo(1));
     res.send({
         message: `Core API: Hello, ${req.query.name || 'World'}!`
     });
--- libs/validator/index.js
+++ libs/validator/index.js
@@ -1,2 +1,3 @@
 exports.isNullOrWhitespace = (value) => value === undefined || value === null || !value.trim();
 exports.isOne = (value) => value === 1;
+exports.isTwo = (value) => value === 2;
