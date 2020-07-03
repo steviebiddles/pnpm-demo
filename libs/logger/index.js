eb0fdb85fa0d3d8f3a85bbc613098a773ca7904f
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-07-03 21:49:44 +0100

    no blue
    this is the deatil commit part

--- libs/logger/index.js
+++ libs/logger/index.js
@@ -2,7 +2,6 @@ const CYAN = '\x1b[36m';
 const RED = '\x1b[31m';
 const GREEN = '\x1b[32m';
 const YELLOW = '\x1b[33m';
-const BLUE = '\x1b[34m';
 
 const log = (color, ...args) => console.log(color + '%s', ...args);
 
@@ -10,4 +9,3 @@ exports.info = (...args) => log(CYAN, ...args);
 exports.warn = (...args) => log(YELLOW, ...args);
 exports.error = (...args) => log(RED, ...args);
 exports.success = (...args) => log(GREEN, ...args);
-exports.debug = (...args) => log(BLUE, ...args);
