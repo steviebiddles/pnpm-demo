1a81275ae083b009102551eb2aa0f6101157106a
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-07-03 11:44:44 +0100

    five

--- libs/validator/index.js
+++ libs/validator/index.js
@@ -3,3 +3,4 @@ exports.isOne = (value) => value === 1;
 exports.isTwo = (value) => value === 2;
 exports.isThree = (value) => value === 3;
 exports.isFour = (value) => value === 4;
+exports.isFive = (value) => value === 5;
