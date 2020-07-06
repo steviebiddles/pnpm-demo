973403c0b58d68e9eefa07c48ebdb995455165b1
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-07-06 21:23:04 +0100

    feat: is ten

--- libs/validator/index.js
+++ libs/validator/index.js
@@ -2,3 +2,4 @@ exports.isNullOrWhitespace = (value) => value === undefined || value === null ||
 exports.isOne = (value) => value === 1;
 exports.isTwo = (value) => value === 2;
 exports.isThree = (value) => value === 3;
+exports.isTen = (value) => value === 10;
