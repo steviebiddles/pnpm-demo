b55169ef232e7b4f3d68f0ac932f08fe6d9d95f9
Stephen McAuley <stephen.mcauley@bazaarvoice.com>
2020-07-06 21:15:21 +0100

    lib only

--- libs/validator/index.js
+++ libs/validator/index.js
@@ -1,3 +1,4 @@
 exports.isNullOrWhitespace = (value) => value === undefined || value === null || !value.trim();
 exports.isOne = (value) => value === 1;
 exports.isTwo = (value) => value === 2;
+exports.isThree = (value) => value === 3;
